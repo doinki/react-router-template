384ba0785f3f08815095f22d31f90d9a8d631975
doinki <doinki94@gmail.com>
2025-01-29 22:59:03 +0900

    ✨

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import devServer, { defaultOptions } from '@hono/vite-dev-server';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { envOnlyMacros } from 'vite-env-only';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ isSsrBuild }) => {
@@ -11,6 +12,7 @@ export default defineConfig(({ isSsrBuild }) => {
       target: isSsrBuild ? 'node22' : ['chrome120', 'safari16.4', 'firefox128'],
     },
     plugins: [
+      envOnlyMacros(),
       tsconfigPaths(),
       tailwindcss(),
       reactRouter(),
